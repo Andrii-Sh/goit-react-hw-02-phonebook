1ab4060d6654b5d15bc2b7196d9a2b40df45c637
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-05-07 12:14:46 +0300

    completed step 5 of task 02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,18 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { nanoid } from 'nanoid';
 
+const INITIAL_STATE = {
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+  filter: '',
+};
+
 export class App extends Component {
-  state = {
-    contacts: [],
-    // name: '',
-  };
+  state = { ...INITIAL_STATE };
 
   addContact = ({ name, number }) => {
     const contact = {
@@ -17,13 +24,31 @@ export class App extends Component {
       number,
     };
 
+    if (
+      this.state.contacts.find(
+        item => item.name.toLowerCase() === contact.name.toLowerCase()
+      )
+    ) {
+      alert(`${contact.name} is already in contacts.`);
+      return;
+    }
+
     this.setState(prevstate => ({
       contacts: [contact, ...prevstate.contacts],
     }));
   };
 
+  handleFilterChange = evt => {
+    this.setState({ filter: evt.currentTarget.value });
+  };
+
   render() {
-    const { contacts } = this.state;
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const filtredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
     return (
       <div
         style={{
@@ -38,8 +63,8 @@ export class App extends Component {
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
-        <Filter />
-        <ContactList contacts={contacts} />
+        <Filter value={filter} onChange={this.handleFilterChange} />
+        <ContactList contacts={filtredContacts} />
       </div>
     );
   }
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,3 +1,10 @@
 // import PropTypes from 'prop-types';
 
-export const Filter = () => {};
+export const Filter = ({ onChange, filter }) => {
+  return (
+    <label>
+      Find contacts by name
+      <input type="text" value={filter} onChange={onChange} />
+    </label>
+  );
+};
